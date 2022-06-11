65343e13152695ceb8508906b336e547a8673a41
unknown <quangzzzzz2710@gmail.com>
2022-06-11 17:16:04 +0700

    add phase 3

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const phase1 = document.getElementById ("phase1")
 const btn = document.getElementById ("button-85")
 const phase2 = document.getElementById ("phase2")
+const phase3 = document.getElementById ("phase3")
+const btnAccept = document.getElementById ("button-49")
 // const btnRefuse = document.getElementById ("button-66")
 phase2.style.display = "none"
+phase3.style.display = "none"
 btn.addEventListener ("click",()=>{
     phase1.style.display = "none"
     phase2.style.display = "block"
@@ -13,3 +16,7 @@ $('#button-66').on('mouseover', function() {
       'left': Math.random() * window.innerWidth - 350+ "px"
     } );
   })
+btnAccept.addEventListener ("click",()=>{
+    phase2.style.display = "none"
+    phase3.style.display = "block"
+})
